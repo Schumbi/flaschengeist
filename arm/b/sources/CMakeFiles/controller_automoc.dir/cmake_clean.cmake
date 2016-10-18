ec3eae0ca848d411baa81931ecb1c66cdb65882e
Ralf Warmuth <r.warmuth@schumbi.de>
2016-10-19 00:27:17 +0200

    arm init

--- arm/b/sources/CMakeFiles/controller_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "controller_automoc.cpp"
-  "CMakeFiles/controller_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/controller_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
